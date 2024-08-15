8a685fc43bea0c6e36a15d485cec2c9ae12be86c
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 01:33:15 +0530

    --- Auto Git Commit ---

--- project/output.tf
+++ project/output.tf
@@ -1,10 +1,10 @@
-#output "instance_id" {
- #   value = aws-instance.invor_instance.instance_id
+# #output "instance_id" {
+#  #   value = aws-instance.invor_instance.instance_id
   
-#}
+# #}
 
 
 
-output "instance_id" {
-  value = aws_instance.invor_instance.id
-}
+# output "instance_id" {
+#   value = aws_instance.invor_instance.id
+# }
