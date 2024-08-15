8b61f477a4aa84003a5d29113acfdde346848469
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 01:03:35 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "this" {
     ami = var.instance_ami
-    instance_type = var 
+    instance_type = var.instance_type 
     tags = {
         name = "invor_instance"
     }
