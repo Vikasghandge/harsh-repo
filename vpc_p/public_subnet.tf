943e2926da9fe8ce4658fbed68336a4f9927db9d
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 14:22:17 +0530

    --- Auto Git Commit ---

--- vpc_p/public_subnet.tf
+++ vpc_p/public_subnet.tf
@@ -1,8 +1,10 @@
 resource "aws_subnet" "main" {
 vpc_id = aws_vpc.main.id  
 cidr_block = "10.0.1.0/24"
-map_ = true
+map_public_ip_on_launch = true
 tags = {
   name = "private_subnet"
 }
 }
+
+mao
