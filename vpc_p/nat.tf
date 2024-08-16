b757908379160298a5250799dd1b0e2024240496
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 14:53:12 +0530

    --- Auto Git Commit ---

--- vpc_p/nat.tf
+++ vpc_p/nat.tf
@@ -1,7 +1,7 @@
-# # resource "aws_nat_gateway" "nat" {
-# #     vpc = aws_vpc.main.id
-# #     tags = "nat"
-# # }
+#  resource "aws_nat_gateway" "nat" {
+#      vpc = aws_vpc.main.id
+#      tags = "nat"
+#  }
 
 
 # resource "aws_eip" "nat" {
