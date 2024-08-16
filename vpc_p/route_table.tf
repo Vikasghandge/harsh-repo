5e89edbe4f1ce858643676691b2276e52ad0d2af
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 16:36:33 +0530

    --- Auto Git Commit ---

--- vpc_p/route_table.tf
+++ vpc_p/route_table.tf
@@ -5,6 +5,10 @@ resource "aws_route_table" "rt" {
      route {
         cidr_block = 0.0.0.0/0
         gateway_id = aws_internet_gateway.igw.id
-        
+
      }  
+
+     tags = {
+       name = 
+     }
 }
