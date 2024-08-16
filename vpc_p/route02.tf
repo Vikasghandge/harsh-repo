a52fb15a4771359389053256570467e54bb3629c
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 14:32:20 +0530

    --- Auto Git Commit ---

--- vpc_p/route02.tf
+++ vpc_p/route02.tf
@@ -1,6 +1,6 @@
 # attaching nat gateway
 
-resource "aws_route_table" "rt2" {
+resource "aws_route_table" "rt3" {
     vpc_id = aws_nat_gateway.nat.id 
     route = {
         cidr_block = "0.0.0.0/0"
