9c3a68cfa1ee0bd98563c62f9a0d51baf1690018
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 16:30:31 +0530

    --- Auto Git Commit ---

--- vpc_p/vpc.tf
+++ vpc_p/vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "main" {
     name = "vpc"
   }  
 }
+
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc"
+  }
+}
