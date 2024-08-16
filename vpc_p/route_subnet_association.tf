444809b891c101a8dc9ed2f8bef867091bf78e6c
vikasghandge <ghandgevikas804@gmail.com>
2024-08-16 16:19:13 +0530

    --- Auto Git Commit ---

--- vpc_p/route_subnet_association.tf
+++ vpc_p/route_subnet_association.tf
@@ -1,5 +1,1 @@
-resource "aws_route_table_association" "a" {
-    subnet_id = aws_subnet.main.id
-    route_table_id = aws_route_table_association.id
-  
-}
+resource "aws_route_table_association" "a" 
