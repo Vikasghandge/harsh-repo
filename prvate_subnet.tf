72d1fe6826c4bfb1cb32e9393fe464cc6df6d9c3
vikasghandge <ghandgevikas804@gmail.com>
2024-08-17 19:22:07 +0530

    --- Auto Git Commit ---

--- prvate_subnet.tf
+++ prvate_subnet.tf
@@ -1,4 +1,10 @@
-resource "aws_subnet" "origin" {
-    vpc_id = vpc_id.origin.id
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+      name = "public_subnet"
+    }
   
 }
