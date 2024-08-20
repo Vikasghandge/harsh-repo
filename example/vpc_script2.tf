58b2c7adcb2c8f2cc2c9fc54ff47b400bc2ffa8f
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 12:29:03 +0530

    --- Auto Git Commit ---

--- example/vpc_script2.tf
+++ example/vpc_script2.tf
@@ -41,7 +41,7 @@ resource "aws_internet_gateway" "internet" {
 
 resource "aws_route_table" "public_rt" {
     vpc_id = aws_vpc.vnet.id
-route= {
+route  {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet.id 
 } 
