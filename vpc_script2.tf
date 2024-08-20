a5a0870161c49fdd5e0d4dfdddf25e8f1acfe7f4
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 11:53:53 +0530

    --- Auto Git Commit ---

--- vpc_script2.tf
+++ vpc_script2.tf
@@ -76,6 +76,7 @@ resource "aws_eip" "elastic" {
         cidr_block = "0.0.0.0./0"
         nat_gateway_id = aws_nat_gateway.example.id             
     }
+    tags = 
    
  }
 resource "" "name" {
