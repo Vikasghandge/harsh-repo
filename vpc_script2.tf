bf52de633c421d1217bf8b726e55fdf70102925c
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 11:52:23 +0530

    --- Auto Git Commit ---

--- vpc_script2.tf
+++ vpc_script2.tf
@@ -74,7 +74,7 @@ resource "aws_eip" "elastic" {
     vpc_id = aws_vpc.vnet.id
     route = {
         cidr_block = "0.0.0.0./0"
-         nat_gateway_id =
+        nat_gateway_id = 
               
     }
    
