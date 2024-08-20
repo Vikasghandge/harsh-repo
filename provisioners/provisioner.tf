e995f91250f5f99b1b5d8029f161f7ec3fdd69a2
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 22:57:44 +0530

    --- Auto Git Commit ---

--- provisioners/provisioner.tf
+++ provisioners/provisioner.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2_instance" {
     connection {
         type = "ssh"
         user = "ubuntu"
-        private_key = file("${path.modules}/devops-key")
+        private_key = file("${path.pro}/devops-key")
         host = "${self.public_ip}"
     }
 
