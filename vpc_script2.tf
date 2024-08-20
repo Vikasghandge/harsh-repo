09104c58ff47c48bf01e453de6f4a23ef4065381
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 10:38:19 +0530

    --- Auto Git Commit ---

--- vpc_script2.tf
+++ vpc_script2.tf
@@ -24,6 +24,9 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "192.168.1.0/24"
-    availability_zone = "ap-south"
+    availability_zone = "ap-south-1b"
+    tags = {
+      name = "private"
+    }
   
 }
