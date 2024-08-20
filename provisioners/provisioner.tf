86ee5b163ef2f275706242fa844e6845081ccac9
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 23:01:25 +0530

    --- Auto Git Commit ---

--- provisioners/provisioner.tf
+++ provisioners/provisioner.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 
 # resource block for server creation
-resource "aws_instance" "server_1" {
+resource "aws_instance" "server-1" {
      ami = "ami-0ad21ae1d0696ad58"
      instance_type = "t2.micro"
      key_name = "devops-key"
