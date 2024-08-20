bc48408c3160e2f028fcec2f225496410b10c130
vikasghandge <ghandgevikas804@gmail.com>
2024-08-20 10:55:13 +0530

    --- Auto Git Commit ---

--- vpc_script2.tf
+++ vpc_script2.tf
@@ -43,7 +43,7 @@ resource "aws_route_table" "public_rt" {
     vpc_id = aws_vpc.vpc.id
 route = {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet
+    gateway_id = aws_internet_gateway.internet.id
 } 
  }  
 
